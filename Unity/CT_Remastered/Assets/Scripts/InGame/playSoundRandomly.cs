517a92fac0320f4739dde51c2b0a25f0b9a83563
LisaIz <lisa.izzouzi@gmail.com>
2018-10-18 00:18:49 +0200

    fixed luminosity and sound

---
+++ Unity/CT_Remastered/Assets/Scripts/InGame/playSoundRandomly.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundRandomly : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public AudioClip shootClip;
+
+    void Start()
+    {
+        
+        float index = Random.Range(0, shootClip.length);
+        audioSource.clip = shootClip;
+        audioSource.Play();
+    }
+    
+}
